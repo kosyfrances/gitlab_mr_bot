5c6f69179325e6e9fb3c2f207e5721c32613e630
Kosy Anyanwu <kosyfrances@gmail.com>
2017-07-23 20:12:09 +0100

    Refactor

--- plugins/mr_queue_plugin.py
+++ plugins/mr_queue_plugin.py
@@ -72,46 +72,36 @@ class MrQueueHelper:
 
         return unassigned_mrs
 
+    def render_queue(self, message, method):
+        message_text_list = message.body['text'].lower().split()
+        if len(message_text_list) < 3:
+            error = 'Howdy, please enter a project name'
+            response = render('mr_queue_response.j2', error=error)
+        else:
+            project_name = message_text_list[2:]
+            context = {
+                'mrs': method(project_name)
+            }
+            if context['mrs']:
+                response = render('mr_queue_response.j2', context)
+            else:
+                error = ("Hey there, it is either there is no assigned "
+                         "merge requests or you entered a wrong project name.")
+                response = render('mr_queue_response.j2', error=error)
+        return response
+
 
 @listen_to('queue unassigned', re.IGNORECASE)
 @respond_to('queue unassigned', re.IGNORECASE)
 def queue_unassigned(message):
-    message_text_list = message.body['text'].lower().split()
-    if len(message_text_list) < 3:
-        error = 'Howdy, please enter a project name'
-        response = render('mr_queue_response.j2', error=error)
-    else:
-        project_name = message_text_list[2:]
-        helper = MrQueueHelper()
-        context = {
-            'mrs': helper.get_unassigned_mrs(project_name)
-        }
-        if context['mrs']:
-            response = render('mr_queue_response.j2', context)
-        else:
-            error = ("Hey there, it is either there is no assigned "
-                     "merge requests or you entered a wrong project name.")
-            response = render('mr_queue_response.j2', error=error)
+    helper = MrQueueHelper()
+    response = helper.render_queue(message, helper.get_unassigned_mrs)
     message.reply(response)
 
 
 @listen_to('queue assigned', re.IGNORECASE)
 @respond_to('queue assigned', re.IGNORECASE)
 def queue_assigned(message):
-    message_text_list = message.body['text'].lower().split()
-    if len(message_text_list) < 3:
-        error = 'Howdy, please enter a project name'
-        response = render('mr_queue_response.j2', error=error)
-    else:
-        project_name = message_text_list[2:]
-        helper = MrQueueHelper()
-        context = {
-            'mrs': helper.get_assigned_mrs(project_name)
-        }
-        if context['mrs']:
-            response = render('mr_queue_response.j2', context)
-        else:
-            error = ("Hey there, it is either there is no assigned "
-                     "merge requests or you entered a wrong project name.")
-            response = render('mr_queue_response.j2', error=error)
+    helper = MrQueueHelper()
+    response = helper.render_queue(message, helper.get_assigned_mrs)
     message.reply(response)
